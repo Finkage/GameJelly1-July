ebf36ed0229e6829d4a799a2cd7a5c6b20d4f0f9
Alinda <Alindah@users.noreply.github.com>
2022-08-06 15:13:08 -0400

    Fixed bug where player moves when map is up

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,17 +10,24 @@ public class PlayerMovement : MonoBehaviour
     private float verticalInput;
     private Vector3 moveDirection;
     private Rigidbody rb;
+    private float moveSpeedCopy;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        moveSpeedCopy = moveSpeed;
     }
 
     private void Update()
     {
         if (ViewMap.mapIsUp)
+        {
+            // Stop player movement if mid-movement
+            moveSpeed = 0;
+            rb.velocity = Vector3.zero;
             return;
+        }
 
         UpdateInput();
     }
@@ -32,6 +39,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateInput()
     {
+        moveSpeed = moveSpeedCopy;
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
     }
